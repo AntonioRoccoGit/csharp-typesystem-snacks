eef116d4938d4bc1533a3880f4a8ccb0b704240f
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-09-06 14:36:43 +0200

    added snack2 & snack3

--- csharp-typesystem-snacks/Program.cs
+++ csharp-typesystem-snacks/Program.cs
@@ -24,6 +24,7 @@ namespace csharp_typesystem_snacks
             switch (choiceGame)
             {
                 case 1:
+                    //Snack 1 : L’utente inserisce due numeri in successione.Il software stampa il maggiore.
                     int firstNumber;
                     int secondNumber;
 
@@ -45,10 +46,51 @@ namespace csharp_typesystem_snacks
                     }
 
                     break;
+
                 case 2:
+                    //Snack 2 : L’utente inserisce due parole in successione.Il software stampa prima la parola più corta, poi la parola più lunga.
+                    string firstWord;
+                    string secondWord;
+                    string message = "";
+
                     Console.WriteLine("-----------------------------------Ordina Parole------------------------------------");
+                    Console.WriteLine("L’utente inserisce due parole, verranno visualizzate, in lunghezza crescente");
+                    Console.WriteLine();
+                    Console.WriteLine("La prima parola: ");
+                    firstWord = Console.ReadLine();
+                    Console.WriteLine("La seconda parola: ");
+                    secondWord = Console.ReadLine();
 
+                    if (firstWord.Length > secondWord.Length)
+                    {
+                        message = secondWord + ", " + firstWord;
+                    }
+                    else if (secondWord.Length > firstWord.Length)
+                    {
+                        message = firstWord + ", " + secondWord;
+                    } 
+                    else
+                    {
+                        message = $"Le parole sono lunghe uguali: {firstWord}, {secondWord}";
+                    }
 
+                    Console.WriteLine(message);
+                    break;
+
+                case 3:
+                    //Snack 3: Il software deve chiedere per 10 volte all’utente di inserire un numero.Il programma stampa la somma di tutti i numeri inseriti.
+                    int totalSum = 0;
+
+                    Console.WriteLine("-----------------------------------Somma Pazza------------------------------------");
+                    Console.WriteLine("Inserire 10 numeri, ne verrà fuori la somma");
+
+                    for (int i = 0; i < 10; i++ )
+                    {
+                        Console.WriteLine($"Digitare il {i+1}° numero");
+                        totalSum = totalSum + int.Parse(Console.ReadLine());
+                    }
+
+                    Console.WriteLine($"La somma di tutti i numeri è: {totalSum}");
                     break;
 
             }
@@ -56,8 +98,6 @@ namespace csharp_typesystem_snacks
 
 
 
-            //Snack 2 : L’utente inserisce due parole in successione.Il software stampa prima la parola più corta, poi la parola più lunga.
-            //Snack 3: Il software deve chiedere per 10 volte all’utente di inserire un numero.Il programma stampa la somma di tutti i numeri inseriti.
             //Snack 4: Calcola la somma e la media dei numeri da 2 a 10.
             //Snack 5: Il software chiede all’utente di inserire un numero.Se il numero inserito è pari, stampa il numero, se è dispari, stampa il numero successivo.
             //Snack 6: In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby. Chiedi all’utente il suo nome e comunicagli se può partecipare o meno alla festa.
